91042cc1981cc615f4b16d6441d6c38522eb5818
hftamayo <hftamayo@gmail.com>
2022-06-01 17:58:24 -0600

    updating application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+#en el video: class ApplicationController < ActionController::Base
 class ApplicationController < ActionController::API
+    skip_before_action :authorize_request, only: :authenticate, raise: false
+
+    include Response
+    include ExceptionHandler
+
+    #called before every action on controllers
+    before_action :authorize_request
+    attr_reader :current_user
+
+    private
+
+    #check for valid request token and return user
+    def authorize_request
+        @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+    end
 end
