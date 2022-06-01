29fedc74a33920ed946833302a52b344dba1301b
hftamayo <hftamayo@gmail.com>
2022-06-01 11:55:27 -0600

    adding basic auth to controller project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,10 @@
 class ProjectsController < ApplicationController
+
+    http_basic_authenticate_with name: "hftamayo", password: "secret"
+
     #get /projects
     def index
         @projects = Project.all
         render json: @projects
+    end
 end
