2632ac3e99972ca0bc4f03ac83a8e6fe68dcbdb2
hftamayo <hftamayo@gmail.com>
2022-06-07 12:11:46 -0600

    adding json function to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,8 @@ class Project < ApplicationRecord
 
     #validations
     validates_presence_of :title, :created_by
+
+    def as_json(options = nil)
+        super(:include => [:milestones])
+    end
 end
