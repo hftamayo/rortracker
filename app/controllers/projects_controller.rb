841d7816c1d315ba3a83dcfc481e1fbe3de022cc
hftamayo <hftamayo@gmail.com>
2022-06-07 11:55:06 -0600

    updating project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,24 @@
 class ProjectsController < ApplicationController
+    skip_before_action :verify_authenticity_token, :only => :create
 
-    http_basic_authenticate_with name: "hftamayo", password: "secret"
+    #http_basic_authenticate_with name: "hftamayo", password: "secret"
 
     #get /projects
     def index
-        @projects = Project.all
-        render json: @projects
+        @projects = current_user.projects
+        respond_to do |format|
+            format.html
+            format.json { render json: @projects.to_json(include: :milestones) }
+        end
+    end
+
+    def create
+        @project = current_user.projects.create!(project_params)
+        json_response(@project, :created)
+    end
+
+    private
+    def project_params
+        params.permit(:title)
     end
 end
